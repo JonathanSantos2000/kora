b53c84cf9d113f1dddc1bf6c2ea004b641c384a2
JonathanSantos2000 <jonathancosta2100@gmail.com>
2025-05-16 22:59:50 -0300

    Feet: criado tela de Login

--- frontend/src/app/components/pages/auth/login/login.component.ts
+++ frontend/src/app/components/pages/auth/login/login.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
+import { TextInputComponent } from '../../../partials/form/text-input/text-input.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, TextInputComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  isSubmitted: boolean = false;
 
+  returnUrl: string = '';
+  constructor(
+    private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+    this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'];
+  }
+  get fc() {
+    return this.loginForm.controls;
+  }
+
+  submit() {}
 }
