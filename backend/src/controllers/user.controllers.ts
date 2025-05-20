40b554c356eb7e4272e8d4e8dd8987025895de03
JonathanSantos2000 <jonathancosta2100@gmail.com>
2025-05-19 22:04:53 -0300

    feet: Endpoints para CRUD de usuários BackEnd

---
+++ backend/src/controllers/user.controllers.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import * as userService from "../services/user.services";
+
+export const register = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const user = await userService.createUser(req.body);
+    res.status(201).json(user);
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const token = await userService.loginUser(req.body);
+    res.status(200).json({ token });
+  } catch (error: any) {
+    res.status(401).json({ error: error.message });
+  }
+};
