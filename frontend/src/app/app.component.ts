44b83bb84ef534e437a93eaea83eaa8d2de85dfd
JonathanSantos2000 <jonathancosta2100@gmail.com>
2025-07-01 00:14:53 -0300

    Feet: Criação menus esquerdo e de adição

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuLeftComponent } from "./components/partials/menu/menu-left/menu-left.component";
 import { MenuTopComponent } from "./components/partials/menu/menu-top/menu-top.component";
+import { MenuAddComponent } from "./components/partials/menu/menu-add/menu-add.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuLeftComponent, MenuTopComponent],
+  imports: [RouterOutlet, MenuLeftComponent, MenuTopComponent, MenuAddComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/src/app/components/partials/menu/menu-add/menu-add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-add',
+  imports: [],
+  templateUrl: './menu-add.component.html',
+  styleUrl: './menu-add.component.css'
+})
+export class MenuAddComponent {
+
+}
